ae8071bddc569efc20a7f1c175d362408bca7368
donamgunner <dydwns9310@gmail.com>
2022-07-21 17:10:51 +0900

    느낀점
    두가지 메소드를 배웠다
    1. 문자열을 소문자로 변환시켜주는 메소드
    
    .toLowerCase()
    
    ex)
    var tempText = "My Awesome String";
    
    console.log( tempText.toLowerCase() );
    
    2.문자열에서 특정 문자 개수 구하는 메소드
    
    match 함수
    
    ex)
    
    var str = 'HTML,CSS,JavaScript';
    var result = str.match(/,/g).filter(item => item !== '');
    
    result
    [',',',']
    
    궁금한점이 하나 더 생겼다 => 이것은 언제쓰는것이고 무슨뜻일까 한번 검색해봐야겠다.

---
+++ 문자열 내 p와 y의 개수.js
@@ -0,0 +1,13 @@
+function solution(s) {
+    var answer = true;
+  let str = s.toLowerCase();
+  let count = 0
+  for (let i = 0; i <s.length; i++) {
+    if (str[i] === 'p') count ++;
+    else if (str[i] ==='y') count--;
+  }
+  return count === 0 ? true : false;
+
+
+    return answer;
+}
